66286a709a363553478bca81778b302c2ce85cba
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-11-16 14:29:54 -0600

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [image, setImage] = useState();
+    const [altText, setAltText] = useState('');
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => {
+            setImage(data.message);
+            setAltText("A Random Dog");
+        });
+
+
+    }, []);
+
+    if (!image) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src={image} alt={altText} />
+        </div>
+    )
+}
+
+export default App;
